d1c181855ad53c5988662845f84ffd0e03e3f5fb
root <root@localhost.localdomain>
2016-06-08 02:50:02 -0400

    Add how to use Array

---
+++ TestArray.scala
@@ -0,0 +1,21 @@
+object TestArray {
+	def main(args:Array[String]){
+		var myList = Array(1.9, 2.9, 3.4, 3.5)
+
+		for (x<-myList) {
+			println(x)
+		}
+
+		var total = 0.0
+		for (i <- 0 to (myList.length - 1)) {
+			total += myList(i);
+		}
+		println("Total is " + total)
+
+		var max = myList(0)
+		for(i <-1 to (myList.length - 1)) {
+			if (myList(i) > max) max = myList(i)
+		}
+		println("Max is " + max)
+	}
+}
