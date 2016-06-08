b6d1023f96b69a6c66f8a4c829defc2defba0e6a
fifaquake <lipengweb@gmail.com>
2016-06-08 03:54:05 -0400

    Add sub class

--- TestClass.scala
+++ TestClass.scala
@@ -1,4 +1,5 @@
-class Point(xc: Int, yc : Int) {
+import java.io._
+class Point(val xc: Int, val yc : Int) {
 	var x: Int = xc
 	var y: Int = yc
 
@@ -11,9 +12,26 @@ class Point(xc: Int, yc : Int) {
 	}
 }
 
+class Location(override val xc : Int, override val yc : Int, val zc : Int) extends Point(xc, yc) {
+	var z: Int = zc
+
+	def move(dx: Int, dy: Int, dz :Int) {
+		x = x + dx
+		y = y + dy
+		z = z + dz
+
+		println("Pont x location: " + x)
+		println("Pont y location: " + y)
+		println("Pont z location: " + z)
+	}
+}
+
 object TestClass {
 	def main(args: Array[String]) {
 		val pt = new Point(10, 20)
 		pt.move(10, 10)
+
+		val loc = new Location(10, 20, 15)
+		loc.move(10, 10, 5)
 	}
 }
