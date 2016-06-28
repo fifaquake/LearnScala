b1842f9c2bf8d603561eec508bd1759b9df549d8
fifaquake <lipengweb@gmail.com>
2016-06-28 05:26:03 -0400

    add samples for singleton

---
+++ Singleton.scala
@@ -0,0 +1,25 @@
+import scala.collection.mutable._
+
+class Marker(val color: String) {
+	println(s"Creating ${this}")
+
+	override def toString=s"marker color $color"
+}
+
+object MarkerFactory {
+	private val markers = Map(
+		"red" -> new Marker("red"),
+		"blue" -> new Marker("blue"),
+		"yellow" -> new Marker("yellow")
+	)
+
+	def getMarker(color:String) = {
+		markers.getOrElseUpdate(color, new Marker(color))
+	}
+} 
+
+println(MarkerFactory getMarker "blue")
+println(MarkerFactory getMarker "blue")
+println(MarkerFactory getMarker "red")
+println(MarkerFactory getMarker "red")
+println(MarkerFactory getMarker "green")
